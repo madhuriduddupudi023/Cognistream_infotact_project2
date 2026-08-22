f649cb80702ae493fb279e702d957e6a61f0a7ae
madhuriduddupudi023 <147141312+madhuriduddupudi023@users.noreply.github.com>
2026-08-22 20:55:34 +0530

    Create db_config.py

---
+++ database/db_config.py
@@ -0,0 +1,9 @@
+# PostgreSQL configuration for Cognistream
+
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "database": "cognistream",
+    "user": "postgres",
+    "password": "postgres"
+}
